4ceecfce4cf0e1313e2cbc89e43a1e960c359882
alancj731 <alanchenjian@gmail.com>
2025-03-28 21:22:37 -0500

    temp save

--- src/components/chatbox.tsx
+++ src/components/chatbox.tsx
@@ -1,6 +1,7 @@
 import Myself from "@/components/myself";
 import { Message, useChat } from "@ai-sdk/react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { User } from "lucide-react";
 
 const initMessage: Message = {
   id: "1",
@@ -23,7 +24,7 @@ export default function ChatBox() {
       const target = initMessage.content;
       const timer = setInterval(() => {
         if (index < target.length) {
-          const current = index
+          const current = index;
           setTypingOutput((prev) => prev + target[current]);
           index++;
         } else {
@@ -50,8 +51,18 @@ export default function ChatBox() {
     handleSubmit(e);
   };
 
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest'
+    });
+  }, [messages]);
+
   return (
-    <div className="items-between space-y-2 stretch z-15 flex h-[640px] w-full flex-col justify-between text-xl h-[calc(100vh-8rem)] md:px-10">
+    <div className="overflow-hidden items-between space-y-2 stretch z-15 flex h-[640px] w-full flex-col justify-between text-xl md:px-10">
+
       <div className="md:w-1/2 md:self-center">
         <div className="items-betweeb flex-col">
           {messages.map((m, index) => (
@@ -59,24 +70,30 @@ export default function ChatBox() {
               key={"message-" + index}
               className={`flex ${
                 m.role === "user" ? "justify-end" : "justify-start"
-              } items-center gap-2 p-2`}
+              } items-start gap-2 p-2`}
             >
               {m.role === "assistant" && (
-                <div className="flex items-center">
+                <div className="flex items-center mt-2">
                   <Myself />
                 </div>
               )}
-              <div className="ml-2 p-2">
+              {m.role === "user" && (
+                <div className="flex mt-1 justify-center items-center h-10 w-10 rounded-full bg-accent ">
+                  <User />
+                </div>
+              )}
+              <div className="py-2">
                 {typing && index === typingIndex ? typingOutput : m.content}
               </div>
             </div>
           ))}
+          <div ref={messagesEndRef} />
         </div>
       </div>
 
       <form
         onSubmit={myHandleSubmit}
-        className="flex justify-center overflow-hidden"
+        className="flex justify-center overflow-y-auto"
       >
         <input
           className="z-10 w-full rounded border border-zinc-300 shadow-xl md:w-1/2 dark:border-zinc-800 dark:bg-zinc-900"
